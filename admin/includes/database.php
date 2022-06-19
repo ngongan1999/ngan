16fa27bfffa0d8829244f04358831375075746ff
ngongan1999 <107790955+ngongan1999@users.noreply.github.com>
2022-06-19 16:20:26 +0700

    hello ca nha iu

--- admin/includes/database.php
+++ admin/includes/database.php
@@ -2,9 +2,9 @@
 class database{
 
 	//DB Params
-	private $dns = "mysql:host=localhost;dbname=blog";
-	private $username = "root";
-	private $password = "";
+	private $dns = "mysql:host=bimldneotnil8ybedvyb-mysql.services.clever-cloud.com;dbname=bimldneotnil8ybedvyb";
+	private $username ="uprilimetqwl4xxv";
+	private $password="WXDtHfxA526SEYtmhM5V";
 	private $conn;
 
 	//DB Connect
